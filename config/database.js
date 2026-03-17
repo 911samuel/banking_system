bddaafaf09c9045e257d822528f08a03a3624f30
@URUJENI123 <urujenideborah80@gmail.com>
2026-03-17 14:34:38 -0700

    initial commit

--- config/database.js
+++ config/database.js
@@ -16,9 +16,9 @@ const sequelize = new Sequelize(
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
-    console.log('✅ PostgreSQL connection has been established successfully.');
+    console.log(' PostgreSQL connection has been established successfully.');
   } catch (error) {
-    console.error('❌ Unable to connect to the database:', error);
+    console.error(' Unable to connect to the database:', error);
   }
 };
 
